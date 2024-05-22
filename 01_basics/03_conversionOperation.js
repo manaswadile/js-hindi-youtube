b9c2849fd23c76be1319d3437671a4f4867dcbec
manaswadile <wadilemanas2006@gmail.com>
2024-05-22 03:04:46 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+// let score =  "manas"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+// let isLoggedIn = "manas"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "manas" => true
+
+
+let someNumber = 34
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
