2ecab3fd9f94d3afb886892aa280e7e4948ddfb0
manaswadile <wadilemanas2006@gmail.com>
2024-05-27 01:10:51 +0000

    array 2

---
+++ 02_basics/02_array.js
@@ -0,0 +1,32 @@
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros);
+
+const all_new_heros = [...marvel_heros, ...dc_heros]  /// this is spread ; 2 or more arrays can merge in single array
+
+// console.log(all_new_heros);
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+const real_another_array = another_array.flat(Infinity) // it forms a new single array with all sub arrays
+
+console.log(real_another_array);
+
+
+
+console.log(Array.isArray("Hitesh")) // it asks que is it array
+console.log(Array.from("Hitesh")) // converts to array
+console.log(Array.from({name: "hitesh"})) // interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3)); 
