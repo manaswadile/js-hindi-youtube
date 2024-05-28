e64142d528da1c0dbce050c5fcca80dbbcbb4712
manaswadile <wadilemanas2006@gmail.com>
2024-05-28 03:59:02 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,7 +3,7 @@ let a = 300
 if (true) {
     let a = 10
     const b = 20
-    console.log("INNER: ", a);
+    // console.log("INNER: ", a);
 }
 
 // console.log(a);
@@ -18,9 +18,37 @@ function one(){
         const website = "youtube"
         console.log(username);
     }
-    console.log(website);
+    // console.log(website);
 
     two()
+
 }
 
-one()
+// one()
+
+if (true) {
+    const username = "manas"
+    if (username === "manas") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+//+++++++++++ interesting ++++++++++++
+ 
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+addTwo(5)
+
+const addTwo = function(num){
+    return num + 2
+}
