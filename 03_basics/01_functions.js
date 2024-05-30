61db8dd8fad4309026198aba7ec206e0f6a51c40
manaswadile <wadilemanas2006@gmail.com>
2024-05-30 03:47:37 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -32,4 +32,36 @@ function loginUserMessage(username = "sam"){
 }
 
 // console.log(loginUserMessage("manas"))
-console.log(loginUserMessage("manas"))
+// console.log(loginUserMessage("manas"))
+
+/// here ... is a rest operator
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000));
+
+const user = {
+    username: "manas",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 600, 100]
+
+function returnSecondValue(getArray){
+    return getArray[3]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 600, 100]));
