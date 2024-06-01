cf1c8d5025a771d4d1ec19a45702a9bf2fbd6024
manaswadile <wadilemanas2006@gmail.com>
2024-06-01 08:43:42 +0000

    revision

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -25,10 +25,6 @@
 
 // console.log(`User power: ${power}`);
 
-
-// const balance = 1000
-
-
 //======================================================================
 
 // if (balance > 500) console.log("test");
@@ -41,15 +37,19 @@
 
 //======================================================================
 
-// if(balance < 500){
-//     console.log("less than 500");
-// }else if (balance < 750){
-//     console.log("less than 750");
-// }else if (balance < 900){
-//     console.log("less than 900");
-// }else {
-//     console.log("less than 1200");
-// }
+
+const balance = 700
+
+
+if(balance < 500){
+    console.log("less than 500");
+}else if (balance < 750){
+    console.log("less than 750");
+}else if (balance < 900){
+    console.log("less than 900");
+}else {
+    console.log("less than 1200");
+}
 
 
 const userLoggedIn = true
@@ -58,9 +58,9 @@ const loggedInFromGoogle = false
 const loggedInFromEmail = true
 
 if (userLoggedIn && debitCard && 2==3) {
-    console.log("Allow to buy courses");
+    // console.log("Allow to buy courses");
 }
 
 if (loggedInFromGoogle || loggedInFromEmail ){
-    console.log("User logged in");
+    // console.log("User logged in");
 }
